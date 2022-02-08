b4a61d4492fb79aba4f4888f882e93ee9cc8c875
anjarfals-07 <anjarfals07@gmail.com>
2022-02-08 17:31:34 +0700

    Update Sourcecode

---
+++ src/main/java/com/anjar/muhammad/variable/TipeDataNumber.java
@@ -0,0 +1,15 @@
+package com.anjar.muhammad.variable;
+
+public class TipeDataNumber {
+
+    public static void main(String[]args){
+        Byte minByte    = -128;
+        Byte maxByte    = 127;
+        Short minShort  = -32_768;
+        Short maxShort  =  32_767;
+
+        System.out.println("Bilangan Byte nilai minimumnya adalah "+minByte+" dan maksimumnya "+maxByte);
+        System.out.println("Bilangan Short nilai minimumnya adalah "+minShort+" dan maksimumnya "+minShort);
+
+    }
+}
